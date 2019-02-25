b9b6dc24ffbd389d07220c6cb5e05acf11427c1f
Lada Kesseler <lada.kesseler1@t-mobile.com>
2019-02-25 09:33:09 -0800

    Fixed heroku port

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
    console.log(`Server is listening on port ${port}`);
 });
