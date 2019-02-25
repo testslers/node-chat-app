ef13a9f7c7cd14b4b5d7babb76b444672225e3bc
Lada Kesseler <lada.kesseler1@t-mobile.com>
2019-02-25 10:15:50 -0800

    Playing around with sockets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public"); // built-in 'path' module
 const port = process.env.PORT || 3000;
 
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-   console.log(`Server is listening on port ${port}`);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnected from server');
+    });
+});
+
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
