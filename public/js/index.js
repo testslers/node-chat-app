117d4ce7538115586d17d82a26e9e5a0b9237cff
Lada Kesseler <lada.kesseler1@t-mobile.com>
2019-02-25 11:06:13 -0800

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+let socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: "some.user@gmail.com",
+        text: "hello to you, too"
+    })
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+   console.log('You have a new message', message);
+});
--- server/server.js
+++ server/server.js
@@ -15,11 +15,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Archie.Kesseler@gmail.com',
+        text: 'Hi from Archie',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log(`User ${message.from} created a new message: ${message.text}`);
+    });
+
     socket.on('disconnect', (socket) => {
         console.log('Disconnected from server');
     });
 });
 
+// newMessage event -- server to the client: {from: '', text: '', createdAt: 123}
+// createMessage - client to the server: {from: '', text: ''}
 
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
